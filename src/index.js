9b1d7bfaeada6c8a1642df2b4151a8f542996812
Fathin Naufaliya <76151903+fathin123@users.noreply.github.com>
2022-03-11 00:02:20 +0700

    menambah BlogPost

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,6 +9,7 @@ import StateFullComponent from './container/StateFullComponent';
 import List from './component/List';
 import Test from './component/Test';
 import Login from './login/Login';
+import BlogPost from './container/BlogPost/BlogPost';
 
 // Default
 // ReactDOM.render(
@@ -56,7 +58,10 @@ import Login from './login/Login';
 // ReactDOM.render(<Test/>, document.getElementById('root'))
 
 // Tugas Pertemuan 3
-ReactDOM.render(<Login/>, document.getElementById('root'));
+// ReactDOM.render(<Login/>, document.getElementById('root'));
+
+// Pertemuan 4
+ReactDOM.render(<BlogPost/>, document.getElementById('content'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
